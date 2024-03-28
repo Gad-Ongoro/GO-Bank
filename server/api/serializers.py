7a2217d8a033af349506f602aca7a2fa49f92cd3
Gad-Ongoro <gadongoro1@gmail.com>
2024-03-28 23:48:13 +0300

    feat: Added API urlpatterns

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -7,52 +7,52 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    # user = UserSerializer()
     class Meta:
         model = models.Profile
         fields = '__all__'
         
 class AddressSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    # user = UserSerializer()
     class Meta:
         model = models.Address
-        fileds = '__all__'
+        fields = '__all__'
         
 class BeneficiarySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Beneficiary
-        fileds = '__all__'
+        fields = '__all__'
         
 class BankAccountSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    beneficiary = BeneficiarySerializer()
+    # user = UserSerializer()
+    # beneficiary = BeneficiarySerializer()
     class Meta:
         model = models.Bank_Account
-        fileds = '__all__'
+        fields = '__all__'
         
 class TransactionSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    bank_account = BankAccountSerializer()
-    beneficiary = BeneficiarySerializer()
+    # user = UserSerializer()
+    # bank_account = BankAccountSerializer()
+    # beneficiary = BeneficiarySerializer()
     class Meta:
         model = models.Transaction
-        fileds = '__all__'
+        fields = '__all__'
         
 class CardSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    # user = UserSerializer()
     class Meta:
         model = models.Card
-        fileds = '__all__'
+        fields = '__all__'
         
-class LoanSerializer():
-    user = UserSerializer()
+class LoanSerializer(serializers.ModelSerializer):
+    # user = UserSerializer()
     class Meta:
         model = models.Loan
-        fileds = '__all__'
+        fields = '__all__'
         
-class LoanPaymentSerializer():
-    loan = LoanSerializer()
-    user = UserSerializer()
+class LoanPaymentSerializer(serializers.ModelSerializer):
+    # loan = LoanSerializer()
+    # user = UserSerializer()
     class Meta:
         model = models.Loan_Payment
-        fileds = '__all__'
+        fields = '__all__'
--- server/api/urls.py
+++ server/api/urls.py
@@ -2,7 +2,39 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Users
+    # User
     path('users/', views.Users_ListCreateApiView.as_view(), name='users_list'),
-    path('users/<uuid:pk>/', views.User_Detail_APIView.as_view(), name='user_detail')
+    path('users/<uuid:pk>/', views.User_Detail_APIView.as_view(), name='user_detail'),
+
+    # Profile
+    path('profiles/', views.Profile_ListCreateAPIView.as_view(), name='profiles_list'),
+    path('profiles/<uuid:pk>/', views.Profile_Detail_APIView.as_view(), name='profile_detail'),
+
+    # Address
+    path('address/', views.Address_ListCreateAPIView.as_view(), name="address_list"),
+    path('address/<uuid:pk>/', views.Address_Detail_APIView.as_view(), name='address_detail'),
+
+    # Beneficiary
+    path('beneficiary/', views.Beneficiary_ListCreateAPIView.as_view(), name="beneficiary_list"),
+    path('beneficiary/<uuid:pk>/', views.Beneficiary_Detail_APIView.as_view(), name='beneficiary_detail'),
+
+    # Bank_Account
+    path('bank_accounts/', views.Bank_Account_ListCreateAPIView.as_view(), name="bank_account_list"),
+    path('bank_account/<uuid:pk>/', views.Bank_Account_Detail_APIView.as_view(), name='bank_account_detail'),
+
+    # Transaction
+    path('transactions/', views.Transaction_ListCreateAPIView.as_view(), name="transaction_list"),
+    path('transaction/<uuid:pk>/', views.Transaction_Detail_APIView.as_view(), name='transaction_detail'),
+
+    # Card
+    path('cards/', views.Card_ListCreateAPIView.as_view(), name="card_list"),
+    path('card/<uuid:pk>/', views.Card_Detail_APIView.as_view(), name='card_detail'),
+
+    # Loan
+    path('loans/', views.Loan_ListCreateAPIView.as_view(), name="loan_list"),
+    path('loan/<uuid:pk>/', views.Loan_Detail_APIView.as_view(), name='loan_detail'),
+    
+    # Loan_Payment
+    path('loan_payments/', views.Loan_Payment_ListCreateAPIView.as_view(), name="loan_payment_list"),
+    path('loan_payment/<uuid:pk>/', views.Loan_Payment_Detail_APIView.as_view(), name='loan_payment_detail'),
 ]
