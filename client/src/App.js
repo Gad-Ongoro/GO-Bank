b708ea257d28e9f4ab60c637457b4679aa669c7f
Gad-Ongoro <gadongoro1@gmail.com>
2024-04-22 22:52:56 +0300

    feat: react-icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import SideNav from './components/SideNav';
 import Main from './components/Main';
 import Header from './components/Header';
 
 function App() {
 	return (
 		<div>
+			<div className='container-fluid d-flex gap-4'>
+				<div>
+					<SideNav></SideNav>
+				</div>
+			</div>
 			<Header></Header>
 			<Main></Main>
 		</div>
---
+++ client/src/components/SideNav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { TbSmartHome } from "react-icons/tb";
+import { IconContext } from 'react-icons/lib';
+
+function SideNav() {
+	return (
+		<div>
+			<IconContext.Provider
+      			value={{ color: 'black', size: '30px' }}
+    		>
+				<TbSmartHome size={30}/>
+			</IconContext.Provider>
+		</div>
+	)
+}
+
+export default SideNav;
