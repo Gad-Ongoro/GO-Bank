194d64fed713830ed178b4829b8947a6e39c07b1
Gad-Ongoro <gadongoro1@gmail.com>
2024-04-28 19:39:19 +0300

    feat: Added footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<footer className='bg-gray-950 text-gray-400 text-sm grid justify-between grid-cols-3 text-center'>
+			<div className='container'>
+				<div>
+					<div>
+						<p>Regulated by the Central Bank of Kenya	|   © 2024 GO Bank.</p>
+					</div>
+					<div><p>All Rights Reserved.</p></div>
+				</div>
+			</div>
+			<div>Social Media Icons</div>
+			<div>Social Media Disclaimer | Terms & Condition | Cookie Policy | Customer Promise | Data Privacy Statement</div>
+		</footer>
+	)
+}
+
+export default Footer;
