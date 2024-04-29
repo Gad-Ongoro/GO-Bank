7b9d7ccad5f3d42ee84ad404058ecf2eeba4e1a7
Gad-Ongoro <gadongoro1@gmail.com>
2024-04-29 22:03:27 +0300

    feat: Added logout logic

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useContext } from 'react';
+import { NavLink, Navigate } from 'react-router-dom';
+import { BankContext } from '../App';
 
 function Header() {
+	let { token_exists, signedIn } = useContext(BankContext);
+
+	function handleLogOut(){
+		localStorage.clear();
+		Navigate('/login')
+	};
+
 	return (
-		<div className=''>
+		<header className='sticky top-0 z-50 backdrop-blur'>
 			<div className='container d-flex justify-content-between'>
 				<div>
-					<h2>GO_Bank</h2>
+					<NavLink 
+						to={'/'}
+						className={'no-underline'}
+					>
+						<h1 className='text-2xl font-bold mt-1 p-0 text-green-500'>GO Bank</h1>
+					</NavLink>
 				</div>
-				<div className='d-flex'>
+				<div className='nav_holder d-flex'>
 					<div className='m-2'>
-						<NavLink className='m-3 text-decoration-none'>About</NavLink>
-						<NavLink className='m-3 text-decoration-none'>Services</NavLink>
-						<NavLink className='m-3 text-decoration-none'>Contact Us</NavLink>
+						<ul className='d-flex list-none'>
+							<li className='navlink'>
+								<NavLink className='navlink m-3 no-underline' to={'/about'}>About</NavLink>
+							</li>
+							<li className='navlink'>
+								<NavLink className='navlink m-3 no-underline' to={'/services'}>Services</NavLink>
+							</li>
+							{/* <li className='navlink'>
+								<NavLink className='navlink m-3 no-underline' to={'/contact'}>Contact Us</NavLink>
+							</li> */}
+						</ul>
 					</div>
 					<div className='m-2'>
-						<NavLink className='m-3 text-decoration-none'>Login</NavLink>
-						<NavLink className='m-3 text-decoration-none'>Signup</NavLink>
+						<ul className='d-flex list-none'>
+							<li className='navlink'>
+								{token_exists || signedIn ? <NavLink to={'/signin'} className='navlink m-3 no-underline' onClick={handleLogOut}>Logout</NavLink> : <NavLink to={'/signin'} className='navlink m-3 no-underline'>Login</NavLink>}
+							</li>
+							{/* <li className='navlink'>
+								<NavLink to={'/register'} className='navlink m-3 no-underline'>Signup</NavLink>
+							</li> */}
+						</ul>
 					</div>
 				</div>
 			</div>
-		</div>
+		</header>
 	)
 }
 
